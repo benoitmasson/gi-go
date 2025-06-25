0e17e78213dba46d4aa5c030c8e9fcad93f3c10a
Benoit Masson <benoit.masson@gmail.com>
2025-06-25 13:19:09 +0200

    Complete demo

--- count.go
+++ count.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"io"
+	"math/rand/v2"
+	"strings"
 )
 
 /* Count total */
 
 func countTotalSheep(r io.Reader) int {
-	count := 0
+	data, err := io.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
 
+	sheepNames := strings.Fields(string(data))
+	count := 0
+	for _, name := range sheepNames {
+		admireSheep(name)
+		count++
+	}
 	return count
 }
 
@@ -27,22 +38,58 @@ func admireSheep(name string) {
 
 func countTotalSheepIterator(r io.Reader) int {
 	count := 0
-
+	for sheepName := range ReadFields(r) {
+		admireSheep(sheepName)
+		count++
+	}
 	return count
 }
 
 /* Count distinct */
 
 func countExactDistinctSheep(r io.Reader) (int, int) {
-	count, distinct := 0, 0
-
-	return count, distinct
+	count := 0
+	sheepNames := make(map[string]bool)
+	for sheepName := range ReadFields(r) {
+		sheepNames[sheepName] = true
+		count++
+	}
+	return count, len(sheepNames)
 }
 
 // countApproxDistinctSheep implements CVM algorithm
 // See https://www.quantamagazine.org/computer-scientists-invent-an-efficient-new-way-to-count-20240516/
 func countApproxDistinctSheep(r io.Reader, memSize int) (int, int, int) {
-	count, distinct, rounds := 0, 0, 0
+	count := 0
+	sheepNames := make(map[string]bool, memSize)
+	currentRound := 0
+	for sheepName := range ReadFields(r) {
+		count++
 
-	return count, distinct, rounds
+		// fmt.Println(w, currentRound, len(words))
+		if rand.Uint64N(1<<currentRound) > 0 {
+			// randomly clear memory
+			// keeping/storing words becomes more and more difficult as rounds go by (two times harder by round)
+			delete(sheepNames, sheepName)
+		} else {
+			sheepNames[sheepName] = true
+		}
+
+		if len(sheepNames) >= memSize {
+			// memory full: cleanup half of it and move to next round
+			cleanup(sheepNames)
+			currentRound++
+		}
+	}
+
+	return count, len(sheepNames) << currentRound, currentRound + 1
+}
+
+// cleanup randomly clears half of the keys
+func cleanup(m map[string]bool) {
+	for k := range m {
+		if rand.IntN(2) == 0 {
+			delete(m, k)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"time"
 )
@@ -36,13 +37,13 @@ func main() {
 	fmt.Println()
 
 	var (
-		m0, m      uint64
-		start      time.Time
-		totalCount int
+		m0, m                             uint64
+		start                             time.Time
+		totalCount, distinctCount, rounds int
 	)
 	m0 = disableGC()
 
-	// Count total //
+	/* Count total */
 	// basic
 	start = time.Now()
 	totalCount = countTotalSheep(f)
@@ -50,92 +51,27 @@ func main() {
 	m = clearMemory(f)
 	fmt.Printf("[CountTotal] Used %dkB heap memory in %dms\n\n", m-m0, time.Since(start).Milliseconds())
 
-	/*
+	// iterator
+	start = time.Now()
+	totalCount = countTotalSheepIterator(f)
+	fmt.Printf("[CountTotalIterator] Found %d total sheep\n", totalCount)
+	m = clearMemory(f)
+	fmt.Printf("[CountTotalIterator] Used %dkB heap memory in %dms\n\n", m-m0, time.Since(start).Milliseconds())
 
+	/* Count distinct */
+	// basic
+	start = time.Now()
+	totalCount, distinctCount = countExactDistinctSheep(f)
+	fmt.Printf("[ExactCountDistinct] Found %d distinct sheep (out of %d total sheep)\n", distinctCount, totalCount)
+	m = clearMemory(f)
+	fmt.Printf("[ExactCountDistinct] Used %dkB heap memory in %dms\n\n", m-m0, time.Since(start).Milliseconds())
+	exactCount := distinctCount
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// iterator
-		start = time.Now()
-		totalCount = countTotalSheepIterator(f)
-		fmt.Printf("[CountTotalIterator] Found %d total sheep\n", totalCount)
-		m = clearMemory(f)
-		fmt.Printf("[CountTotalIterator] Used %dkB heap memory in %dms\n\n", m-m0, time.Since(start).Milliseconds())
-
-		/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// Count distinct //
-		var distinctCount int
-
-		// basic
-		start = time.Now()
-		totalCount, distinctCount = countExactDistinctSheep(f)
-		fmt.Printf("[ExactCountDistinct] Found %d distinct sheep (out of %d total sheep)\n", distinctCount, totalCount)
-		m = clearMemory(f)
-		fmt.Printf("[ExactCountDistinct] Used %dkB heap memory in %dms\n\n", m-m0, time.Since(start).Milliseconds())
-
-		/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		exactCount := distinctCount
-
-		// CVM
-		var rounds int
-
-		start = time.Now()
-		totalCount, distinctCount, rounds = countApproxDistinctSheep(f, *memSize)
-		fmt.Printf("[ApproxCountDistinct(%d)] Estimated %d distinct sheep in %d rounds (out of %d total sheep)\n", *memSize, distinctCount, rounds, totalCount)
-		m = clearMemory(f)
-		fmt.Printf("[ApproxCountDistinct(%d)] Error rate: %.2f%%\n", *memSize, math.Abs(100*(float64(distinctCount-exactCount)/float64(exactCount))))
-		fmt.Printf("[ApproxCountDistinct(%d)] Used %dkB heap memory in %dms\n\n", *memSize, m-m0, time.Since(start).Milliseconds())
-
-	/* */
+	// CVM
+	start = time.Now()
+	totalCount, distinctCount, rounds = countApproxDistinctSheep(f, *memSize)
+	fmt.Printf("[ApproxCountDistinct(%d)] Estimated %d distinct sheep in %d rounds (out of %d total sheep)\n", *memSize, distinctCount, rounds, totalCount)
+	m = clearMemory(f)
+	fmt.Printf("[ApproxCountDistinct(%d)] Error rate: %.2f%%\n", *memSize, math.Abs(100*(float64(distinctCount-exactCount)/float64(exactCount))))
+	fmt.Printf("[ApproxCountDistinct(%d)] Used %dkB heap memory in %dms\n\n", *memSize, m-m0, time.Since(start).Milliseconds())
 }
